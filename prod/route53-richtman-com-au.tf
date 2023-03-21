d58a718dbb2df84ba4210cb391636e5912f16464
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-03-21 19:36:23 +1000

    added icalendar dns for netlify

--- prod/route53-richtman-com-au.tf
+++ prod/route53-richtman-com-au.tf
@@ -116,3 +116,16 @@ resource "aws_route53_record" "food_richtman_com_au_nameservers" {
     "dns4.p01.nsone.net",
   ]
 }
+# Delegate zone for subdomain to Netlify so they can serve valid TLS certificates for custom domain
+resource "aws_route53_record" "icalendar-leave-optimiser_richtman_com_au_nameservers" {
+  zone_id = aws_route53_zone.richtman_com_au.zone_id
+  name    = "icalendar-leave-optimiser.richtman.com.au"
+  type    = "NS"
+  ttl     = 172800
+  records = [
+    "dns1.p01.nsone.net",
+    "dns2.p01.nsone.net",
+    "dns3.p01.nsone.net",
+    "dns4.p01.nsone.net",
+  ]
+}
