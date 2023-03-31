fcf7968f1cd787cddd515abfd9b79d3de2fee993
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-03-31 22:33:14 +1000

    toned down the flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 # Reference https://discourse.nixos.org/t/easy-install-pypi-python-packages-mach-nix-poetry2nix/23825/6
 {
-  description = "Application packaged using poetry2nix";
+  description = "IaC using Nix";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
@@ -9,89 +9,26 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    {
-      # Nixpkgs overlay providing the application
-      overlay = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: prev: {
-          poetry2nix = prev.poetry2nix.overrideScope' (p2nixfinal: p2nixprev: {
-            # pyfinal & pyprev refers to python packages
-            defaultPoetryOverrides = (p2nixprev.defaultPoetryOverrides.extend (pyfinal: pyprev:
-              {
-                ## dodge infinite recursion ##
-                setuptools = prev.python310Packages.setuptools.override {
-                  inherit (pyfinal)
-                    bootstrapped-pip
-                    pipInstallHook
-                    setuptoolsBuildHook
-                  ;
-                };
-
-                setuptools-scm = prev.python310Packages.setuptools-scm.override {
-                  inherit (pyfinal)
-                    packaging
-                    typing-extensions
-                    tomli
-                    setuptools;
-                };
-
-                pip = prev.python310Packages.pip.override {
-                  inherit (pyfinal)
-                    bootstrapped-pip
-                    mock
-                    scripttest
-                    virtualenv
-                    pretend
-                    pytest
-                    pip-tools
-                  ;
-                };
-                ## dodge infinite recursion (end) ##
-
-                bc-jsonpath-ng = pyprev.bc-jsonpath-ng.overridePythonAttrs (old: rec {
-                  propagatedBuildInputs = builtins.filter (x: ! builtins.elem x [ ]) ((old.propagatedBuildInputs or [ ]) ++ [
-                    pyfinal.setuptools
-                  ]);
-                });
-                bc-detect-secrets = pyprev.bc-detect-secrets.overridePythonAttrs (old: rec {
-                  propagatedBuildInputs = builtins.filter (x: ! builtins.elem x [ ]) ((old.propagatedBuildInputs or [ ]) ++ [
-                    pyfinal.setuptools
-                  ]);
-                });
-              }
-            ));
-          });
-        })
-        (final: prev: {
-          # The application
-          myapp = prev.poetry2nix.mkPoetryApplication {
-            projectDir = ./.;
-          };
-          # The env
-          myenv = prev.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-          };
-        })
-      ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix , ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlay ];
+        };
+        poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
         };
       in
       {
-        packages.poetry = nixpkgs.legacyPackages.${system}.python310Packages.poetry;
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            myenv
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            poetryEnv
             terraform
             terragrunt
             awscli2
             poetry
           ];
         };
-      }));
+      }
+    );
 }
