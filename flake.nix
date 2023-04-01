fb4a8572efda642f81dac7be6ea2bdb34aaf55da
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-04-01 13:47:40 +1000

    fixed development environment

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # Reference https://discourse.nixos.org/t/easy-install-pypi-python-packages-mach-nix-poetry2nix/23825/6
 {
   description = "IaC using Nix";
-
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.poetry2nix = {
     url = "github:nix-community/poetry2nix";
@@ -11,32 +11,35 @@
     url = "github:terranix/terranix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, poetry2nix , terranix, ... }:
-      let
-        pkgs = import nixpkgs {
-        };
-        system = "x86_64-linux";
-        poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
-        };
-      in
-      {
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [
-            poetryEnv
-            terraform
-            terragrunt
-            awscli2
-            poetry
-          ];
-          shellHook = ''
-            . <(terraform-docs completion bash)
-            pre-commit install --install-hooks
-          '';
-        };
-        defaultPackage.x86_64-linux = terranix.lib.terranixConfiguration {
-          inherit system;
-          modules = [ ./config.nix ];
-        };
-      };
+  outputs = { self, nixpkgs, poetry2nix , terranix, flake-utils, ... }:
+      flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+            projectDir = ./.;
+          };
+        in
+        {
+          devShells.default = with pkgs; mkShell {
+            buildInputs = [
+                poetryEnv
+                terraform
+                terragrunt
+                awscli2
+                poetry
+              ];
+            shellHook = ''
+              . <(terraform-docs completion bash)
+              pre-commit install --install-hooks
+            '';
+          };
+          defaultPackage = terranix.lib.terranixConfiguration {
+            inherit system;
+            modules = [ ./config.nix ];
+          };
+        }
+      );
 }
