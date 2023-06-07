18f2d7a2042b0593fc01f4a8773d5949276c3991
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-06-07 21:24:10 +1000

    simplified devtooling

--- flake.nix
+++ flake.nix
@@ -1,99 +1,44 @@
-# Reference https://discourse.nixos.org/t/easy-install-pypi-python-packages-mach-nix-poetry2nix/23825/6
 {
-  description = "Application packaged using poetry2nix";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-  inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
-    inputs.nixpkgs.follows = "nixpkgs";
+  description = "aws-bootstrap";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:arichtman/poetry2nix/rdflib-poetry";
   };
-
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    {
-      # Nixpkgs overlay providing the application
-      overlay = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: prev: {
-          poetry2nix = prev.poetry2nix.overrideScope' (p2nixfinal: p2nixprev: {
-            # pyfinal & pyprev refers to python packages
-            defaultPoetryOverrides = (p2nixprev.defaultPoetryOverrides.extend (pyfinal: pyprev:
-              {
-                ## dodge infinite recursion ##
-                setuptools = prev.python310Packages.setuptools.override {
-                  inherit (pyfinal)
-                    bootstrapped-pip
-                    pipInstallHook
-                    setuptoolsBuildHook
-                  ;
-                };
-
-                setuptools-scm = prev.python310Packages.setuptools-scm.override {
-                  inherit (pyfinal)
-                    packaging
-                    typing-extensions
-                    tomli
-                    setuptools;
-                };
-
-                pip = prev.python310Packages.pip.override {
-                  inherit (pyfinal)
-                    bootstrapped-pip
-                    mock
-                    scripttest
-                    virtualenv
-                    pretend
-                    pytest
-                    pip-tools
-                  ;
-                };
-                ## dodge infinite recursion (end) ##
-
-                bc-jsonpath-ng = pyprev.bc-jsonpath-ng.overridePythonAttrs (old: rec {
-                  propagatedBuildInputs = builtins.filter (x: ! builtins.elem x [ ]) ((old.propagatedBuildInputs or [ ]) ++ [
-                    pyfinal.setuptools
-                  ]);
-                });
-                bc-detect-secrets = pyprev.bc-detect-secrets.overridePythonAttrs (old: rec {
-                  propagatedBuildInputs = builtins.filter (x: ! builtins.elem x [ ]) ((old.propagatedBuildInputs or [ ]) ++ [
-                    pyfinal.setuptools
-                  ]);
-                });
-              }
-            ));
-          });
-        })
-        (final: prev: {
-          # The application
-          myapp = prev.poetry2nix.mkPoetryApplication {
-            projectDir = ./.;
-          };
-          # The env
-          myenv = prev.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-          };
-        })
-      ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
+  outputs = {
+    self,
+    nixpkgs,
+    poetry2nix,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
+    (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlay ];
         };
-      in
-      {
-        packages.poetry = nixpkgs.legacyPackages.${system}.python310Packages.poetry;
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            myenv
-            terraform
-            terragrunt
-            awscli2
-            poetry
-            dig
-            infracost
-          ];
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryEnv ;
+        poetryEnv = mkPoetryEnv {
+          projectDir = ./.;
         };
-      }));
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            nativeBuildInputs = [
+              terraform
+              terragrunt
+              awscli2
+              poetry
+              dig
+              infracost
+              poetryEnv
+              checkov
+            ];
+            shellHook = ''
+              pre-commit install --install-hooks
+            '';
+          };
+      }
+    );
 }
